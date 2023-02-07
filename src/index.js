450ab417d1b6e8b6623bed2f7061e112c2d0ede6
Amalya Megerman <amalyamegerman@gmail.com>
2023-02-07 17:50:42 -0500

    add paths, Link

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Root = () => {
+  //click handler function
+  const handleClick = () => {
+    console.log("Thanks for clicking!");
+  };
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">App</Link>
+        <Link to="/users">Users</Link>
+        <Link to="/users/1">Andrew's Profile</Link>
+        <Link to="/" onClick={handleClick}>
+          App with click handler
+        </Link>
+        <Route exact path="/">
+          <App />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/users/:userId">
+          <Profile />
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
